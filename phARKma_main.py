8eba4b1593f16a0c5a6514b08a402f082a94b06b
Pierce Jamieson <jamieson.pierce@gmail.com>
2022-01-19 11:08:58 -0600

    updating execution file

--- phARKma_main.py
+++ phARKma_main.py
@@ -1,4 +1,16 @@
 import trial_updater
+import get_company_data
+import phARKma_utils
+import get_relationships
+import get_trials
+import trial_updater
+import airtable_utils
+import get_condition_category
+import get_arkg_tickers
+import get_details
+import get_commercialized_drugs
+
+
 
 
 trial_updater.check_trial_deprecation_status(True)
